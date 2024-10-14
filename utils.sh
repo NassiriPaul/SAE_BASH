80a45e2ae6168adc3fef9374205c046c10c25e40
kyrann9 <147066053+kyrann9@users.noreply.github.com>
2024-10-14 14:51:56 +0200

    Add files via upload

--- utils.sh
+++ utils.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
 
 function ecrire { # X Y Char
-  printf "\337\33[%d;%dH%s\338" "$1" "$2" "$3"
+    printf "\337\33[%d;%dH%s\338" "$1" "$2" "$3"
 }
 
 
 function charAt {
-  coordX=$1
-  coordY=$2
-  for ((idx_wall=1;idx_wall<=$nbr_walls;idx_wall++)); do
-      wall_x="wall_x_$idx_wall"
-      wall_y="wall_y_$idx_wall"
-      wall_l="wall_l_$idx_wall"
-      wall_L="wall_L_$idx_wall"
-      for ((line=wall_x ; line<wall_x+wall_l ; line++)); do
-          for ((column=wall_y ; column<wall_y+wall_L;column++)); do
-          
-              if [ "${line}" = "${coordX}" ] && [ "${column}" = "${coordY}" ]
-              then
-                  return '1'
-              fi
-          done 
-      done 
-  done
+    coordX=$1
+    coordY=$2
+    for ((idx_wall=1;idx_wall<=$nbr_walls;idx_wall++)); do
+        wall_x="wall_x_$idx_wall"
+        wall_y="wall_y_$idx_wall"
+        wall_l="wall_l_$idx_wall"
+        wall_L="wall_L_$idx_wall"
+        for ((line=wall_x ; line<wall_x+wall_l ; line++)); do
+            for ((column=wall_y ; column<wall_y+wall_L;column++)); do
+                if [ "${line}" = "${coordX}" ] && [ "${column}" = "${coordY}" ]
+                then
+                    return '1'
+                fi
+            done 
+        done 
+    done
     
     for ((x=$raquette_position_x ; x<=$raquette_position_x+$raquette_longeur ; x++)); do
         if [ "${x}" = "${coordX}" ] && [ "${raquette_position_y}" = "${coordY}" ]
