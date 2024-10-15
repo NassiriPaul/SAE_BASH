c15941cd38766f36cbd886efd7408c0c11d40d5a
Nassiri Paul <57369097+NassiriPaul@users.noreply.github.com>
2024-10-15 15:43:14 +0200

    Add files via upload

--- jouer.sh
+++ jouer.sh
@@ -1,47 +1,12 @@
 #!/bin/bash
 
-. ./variables.sh
 . ./raquette.sh
-. ./utils.sh
+. ./niveau.sh
 
-raquette_position_x=5
-raquette_position_y=9
-raquette_longeur=4
-
-#juste pour test déplacement raquette (temporaire)
-clear
-echo "##############"
-for i in {1..10}; do
-  echo "#            #"
-done
-echo "##############"
-
-afficher_raquette
+lire_niveau map.txt
+afficher_niveau
 
 while true; do
-    key=$(get_key)
-
-    case $key in
-        left)
-            if [ $raquette_position_x -gt 1 ]; then
-                raquette_position_x=$((raquette_position_x - 1))
-            fi
-            ;;
-        right)
-            if [ $((raquette_position_x + raquette_longeur)) -lt 12 ]; then
-                raquette_position_x=$((raquette_position_x + 1))
-            fi
-            ;;
-    esac
-
-    clear
-    echo "##############"
-    for i in {1..10}; do
-      echo "#            #"
-    done
-    echo "##############"
-    
-    afficher_raquette
-
-    sleep 0.1
+    read -rsn1 key
+    deplacer_raquette
 done
--- raquette.sh
+++ raquette.sh
@@ -1,18 +1,48 @@
 #!/bin/bash
 
+. ./utils.sh
+
+
 deplacer_raquette() {
+    echo
+    echo $raquette_position_x $raquette_position_y
+    if [ "$key" = "q" ]; then
+        charAt $[$raquette_position_x - 1] $raquette_position_y
+        if [ $? == '4' ]
+        then
+            raquette_position_x=$((raquette_position_x - 1))
+        fi
+    fi
+    if [ "$key" = "d" ]; then
+        charAt $[$raquette_position_x + $raquette_longeur + 1] $raquette_position_y
+        if [ $? == '4' ]
+        then
+            raquette_position_x=$((raquette_position_x + 1))
+        fi
+    fi
+    echo $raquette_position_x $raquette_position_y
     effacer_raquette
-    afficher_raquette
+    #afficher_raquette
 }
 
 effacer_raquette() {
-    ecrire $raquette_position_y $raquette_position_x "    "
+    
+    for ((i=0; i<=raquette_longeur; i++))
+    do
+        ecrire $raquette_position_y $[$raquette_position_x + i] ' '
+    done
+    
 }
 
 afficher_raquette() {
     local raquette=""
-    for ((i=0; i<raquette_longeur; i++)); do
+    for ((i=0; i<raquette_longeur; i++))
+    do
         raquette="${raquette}${symbole_raquette}"
     done
     ecrire $raquette_position_y $raquette_position_x "$raquette"
+}
+
+tester_deplacement() {
+    
 }
